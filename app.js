98c73f8580803bdf6886eac51517acf5e9a0bd2b
lesmadeit <lesmwendwa@gmail.com>
2024-09-12 22:38:31 +0300

    more changes

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+app.use(express.json()); //body will be in json format
 // Use CORS middleware
-app.use(cors());
+app.use(cors()); // tells server to use cross orogin resource sharing
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    const transporter = nodemailer.createTransport({
+app.post('/', (req, res) => {  //configuring a route shows what comes after localhost. parameters are request and response
+    console.log(req.body) //everything in the form fields is in the req.body
+    const transporter = nodemailer.createTransport({ //configure nodemailer(everything in bracket)
         service: "Gmail",
         host: "smtp.gmail.com",
-        port: 465,
+        port: 465, // recipient port
         secure: true,
         auth: {
           user:"lesmwendwa@gmail.com",
@@ -27,9 +27,9 @@ app.post('/', (req, res) => {
       });
       
       // async..await is not allowed in global scope, must use a wrapper
-      async function main() {
+      async function main() {  //
         // send mail with defined transport object
-        const info = await transporter.sendMail({
+        const info = await transporter.sendMail({ //used to send mail
           from: `Portfolio mailing account: <lesmwendwa@gmail.com>`, // my address 
           to: "lesmwendwa@gmail.com", // list of receivers
           cc: `Senders account:${req.body.email}`, //we will cc sender here
@@ -42,10 +42,10 @@ app.post('/', (req, res) => {
        
       }
       
-      main().catch(console.error);
-  res.send(req.body);
+      main().catch(console.error);// catches error
+  res.send(req.body);//used in postman to see the request body
 });
 
-app.listen(port, () => {
+app.listen(port, () => { // hosts port, run server on the port
   console.log(`Server is running at http://localhost:${port}`);
 });
